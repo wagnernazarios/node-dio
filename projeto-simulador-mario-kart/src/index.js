110d9ce6d2a505fbe87889513a4e2202c03056a3
wagnernazarios <wagnernazario124@gmail.com>
2025-07-16 00:36:59 -0300

    corrigindo bug de sintaxe

--- projeto-simulador-mario-kart/src/index.js
+++ projeto-simulador-mario-kart/src/index.js
@@ -47,11 +47,11 @@ async function logRollResult(charactername, block, diceResult, atributte) {
 
 async function playRaceEngine(character1, character2) {
     for (let round = 1; round <= 5; round++) {
-        ' '
-        console.log(`Rodada ${round}`);
+
+        console.log(`RODADA ${round}:`);
         //sortear bloco
         let block = await getRandomBlock();
-        console.log(`Bloco: ${block}`)
+        console.log(`BLOCO: ${block} `)
 
         //rolar os dados
         let diceResult1 = await rollDice();
@@ -74,16 +74,36 @@ async function playRaceEngine(character1, character2) {
             await logRollResult(character2.NOME, "manobrabilidade", diceResult2, character2.MANOBRABILIDADE);
         }
         if (block == "CONFRONTO") {
-            totalTestSkill1 = diceResult1 + character1.PODER;
-            totalTestSkill2 = diceResult2 + character2.PODER;
+            powerResult1 = diceResult1 + character1.PODER;
+            powerResult2 = diceResult2 + character2.PODER;
+
+            console.log(`${character1.NOME}  confrontou o ${character2.NOME}`);
+
             await logRollResult(character1.NOME, "poder", diceResult1, character1.PODER);
             await logRollResult(character2.NOME, "poder", diceResult2, character2.PODER);
+
+
+
+            character1.PONTOS -= powerResult1 > powerResult2 && character1.PONTOS > 0 ? 1 : 0;
+            character2.PONTOS -= powerResult2 > powerResult1 && character2.PONTOS > 0 ? 1 : 0;
+            console.log(powerResult1 === powerResult2 ? "Confronto empatado! Nenhum ponto foi perdido" : "")
+
         }
+
+        if (totalTestSkill1 > totalTestSkill2) {
+            console.log(`${character1.NOME} marcou um ponto`)
+            character1.PONTOS++
+        } else if (totalTestSkill2 > totalTestSkill1) {
+            console.log(`${character2.NOME} marcou um ponto `)
+            character2.PONTOS++
+        }
+        console.log("------------------------------------------------")
     }
 }
 
 (async function main() {
     console.log(`Corrida entre ${player1.NOME} e ${player2.NOME} começando...`);
+    console.log(``);
 
     await playRaceEngine(player1, player2);
 })();
