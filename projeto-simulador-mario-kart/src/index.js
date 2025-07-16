fb9e0de9c3c3fb1e0d709b52139fb237049d5bc6
wagnernazarios <wagnernazario124@gmail.com>
2025-07-15 22:56:02 -0300

    javascript expression class and almost finishing the project

---
+++ projeto-simulador-mario-kart/src/index.js
@@ -0,0 +1,90 @@
+const player1 = {
+    NOME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+};
+
+const player2 = {
+    NOME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+};
+
+async function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+async function getRandomBlock() {
+
+    let random = Math.random();
+    let result;
+
+    switch (true) {
+        case random < 0.33:
+            result = "RETA";
+            break;
+        case random < 0.66:
+            result = "CURVA";
+            break;
+        case random < 100:
+            result = "CONFRONTO";
+            break;
+    }
+
+    return result;
+
+}
+
+
+async function logRollResult(charactername, block, diceResult, atributte) {
+    console.log(`${charactername} rolou o dado de ${block} ${diceResult} + ${atributte} = ${diceResult + atributte}`)
+}
+
+
+async function playRaceEngine(character1, character2) {
+    for (let round = 1; round <= 5; round++) {
+        ' '
+        console.log(`Rodada ${round}`);
+        //sortear bloco
+        let block = await getRandomBlock();
+        console.log(`Bloco: ${block}`)
+
+        //rolar os dados
+        let diceResult1 = await rollDice();
+        let diceResult2 = await rollDice();
+
+        //teste de habilidade
+        let totalTestSkill1 = 0;
+        let totalTestSkill2 = 0;
+
+        if (block == "RETA") {
+            totalTestSkill1 = diceResult1 + character1.VELOCIDADE;
+            totalTestSkill2 = diceResult2 + character2.VELOCIDADE;
+            await logRollResult(character1.NOME, "velocidade", diceResult1, character1.VELOCIDADE);
+            await logRollResult(character2.NOME, "velocidade", diceResult2, character2.VELOCIDADE);
+        }
+        if (block == "CURVA") {
+            totalTestSkill1 = diceResult1 + character1.MANOBRABILIDADE;
+            totalTestSkill2 = diceResult2 + character2.MANOBRABILIDADE;
+            await logRollResult(character1.NOME, "manobrabilidade", diceResult1, character1.MANOBRABILIDADE);
+            await logRollResult(character2.NOME, "manobrabilidade", diceResult2, character2.MANOBRABILIDADE);
+        }
+        if (block == "CONFRONTO") {
+            totalTestSkill1 = diceResult1 + character1.PODER;
+            totalTestSkill2 = diceResult2 + character2.PODER;
+            await logRollResult(character1.NOME, "poder", diceResult1, character1.PODER);
+            await logRollResult(character2.NOME, "poder", diceResult2, character2.PODER);
+        }
+    }
+}
+
+(async function main() {
+    console.log(`Corrida entre ${player1.NOME} e ${player2.NOME} começando...`);
+
+    await playRaceEngine(player1, player2);
+})();
+
