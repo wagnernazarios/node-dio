48d448aab205e2ff7e96b05f64d421128d416df9
wagnernazarios <wagnernazario124@gmail.com>
2025-08-04 08:13:27 -0300

    iniciando o gerador de senhas e criando as variaveis de abientes

--- gerador-qrCode-Ecommerce/src/index.js
+++ gerador-qrCode-Ecommerce/src/index.js
@@ -2,12 +2,13 @@ import prompt from "prompt";
 import chalk from "chalk";
 import mainPrompt from "./prompts/prompt-main.js";
 import createQRCode from "./services/qr-code/create.js";
+import createPassword from "./services/password/create.js";
 
 
 async function main() {
     prompt.get(mainPrompt, async (err, choose) => {
         if (choose.select == 1) await createQRCode();
-        if (choose.select == 2) console.log(chalk.redBright("Voce escolher o password"));
+        if (choose.select == 2) await createPassword();
 
         prompt.start();
     });
---
+++ gerador-qrCode-Ecommerce/src/services/password/create.js
@@ -0,0 +1,7 @@
+import chalk from "chalk"
+
+async function createPassword() {
+    console.log(chalk.yellow("password"))
+}
+
+export default createPassword
